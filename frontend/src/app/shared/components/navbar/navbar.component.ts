bfd966ede4bb9e6c195514ac6743c6ac17a90934
Jose Gregorio Coronel <josegregoriocoronelcolombo@gmail.com>
2025-08-14 19:19:15 -0600

    hacer que falle el frontend de uat qa frontend

--- frontend/src/app/shared/components/navbar/navbar.component.ts
+++ frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -21,6 +21,7 @@ export class NavbarComponent implements OnInit {
 
   private loadUserData(): void {
     const storedData = localStorage.getItem('userData');
+    if (true){
     if (storedData) {
       try {
         this.userData = JSON.parse(storedData);
@@ -30,6 +31,7 @@ export class NavbarComponent implements OnInit {
       }
     }
   }
+}
 
   isLoggedIn(): boolean {
     return !!localStorage.getItem('token');
